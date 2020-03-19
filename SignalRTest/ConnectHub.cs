e0b7f17da9effeb71719ece215dc7fb1c8524d9e
Saithet <Saithet@users.noreply.github.com>
2020-03-19 12:41:11 +0800

    Add project files.

---
+++ SignalRTest/ConnectHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+using SignalRTest.Models;
+using System.Data;
+
+namespace SignalRTest
+{
+    public class ConnectHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+        public void UpdateList(string updateID)
+        {
+            Guid _updatedID = Guid.Parse(updateID);
+            HSBCPSEntities _entities = new HSBCPSEntities();
+            tbl_Program tblProgram = _entities.tbl_Program.Where(x => x.ID == _updatedID).Single();
+            if (tblProgram.Active == true)
+            {
+                tblProgram.Active = false;
+            }
+            else
+            {
+                tblProgram.Active = true;
+            }
+
+            _entities.Entry(tblProgram).State = EntityState.Modified;
+            _entities.SaveChanges();
+
+
+            Clients.All.UpdateList();
+        }
+    }
+}
---
+++ SignalRTest/Controllers/ProgramController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SignalRTest.Models;
+using Newtonsoft.Json;
+
+namespace SignalRTest.Controllers
+{
+    public class ProgramController : Controller
+    {
+        // GET: Program
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Program
+        
+        public ActionResult GetList()
+        {
+            HSBCPSEntities _entities = new HSBCPSEntities();
+
+            IList<tbl_Program> tblProgramList = _entities.tbl_Program.OrderBy(x=>x.Name).ToList();
+
+            string jsonResult = JsonConvert.SerializeObject(tblProgramList);
+
+            DataTableResponse aa = new DataTableResponse
+            {
+                recordsTotal = tblProgramList.Count(),
+                recordsFiltered = 10,
+                data = tblProgramList.Take(10).ToArray()
+            };
+
+            return Json(aa, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public class DataTableResponse
+        {
+            public int draw { get; set; }
+            public long recordsTotal { get; set; }
+            public int recordsFiltered { get; set; }
+            public object[] data { get; set; }
+            public string error { get; set; }
+        }
+    }
+}
